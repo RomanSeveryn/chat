a93f90ecb7a0519259d2514e4b21f5da5961d777
Roman Severyn <romaseverin50@gmail.com>
2023-05-15 10:15:29 +0300

    create page container

---
+++ src/components/PageContainer.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native';
+
+export const PageContainer = ({ children, style }) => {
+  return <View style={{ ...styles.container, ...style }}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+});
