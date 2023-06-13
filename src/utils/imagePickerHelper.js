b7c2e825ca4009d9ca0e4ee166348f5971926178
Roman Severyn <romaseverin50@gmail.com>
2023-06-13 16:28:54 +0300

    add new helper imagePickerHelper

---
+++ src/utils/imagePickerHelper.js
@@ -0,0 +1,29 @@
+import * as ImagePicker from 'expo-image-picker';
+import { Platform } from 'react-native';
+
+export const launchImagePicker = async () => {
+  await checkMediaPermissions();
+
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 1,
+  });
+
+  if (!result.canceled) {
+    return result?.uri;
+  }
+};
+
+const checkMediaPermissions = async () => {
+  if (Platform.OS !== 'web') {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      return Promise.reject('We need permission to access your photos');
+    }
+  }
+
+  return Promise.resolve();
+};
