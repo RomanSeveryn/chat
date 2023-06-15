f9fa59397bea0fb08c3b6ba825227acf62b42d74
Roman Severyn <romaseverin50@gmail.com>
2023-06-15 15:27:09 +0300

    add icon and message for user result

--- src/screens/NewChatScreen.js
+++ src/screens/NewChatScreen.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { CustomHeaderButton } from '../components/CustomHeaderButton';
 import { FontAwesome } from '@expo/vector-icons';
 import { PageContainer } from '../components/PageContainer';
 import colors from '../constants/colors';
+import commonStyles from '../constants/commonStyles';
 
 export const NewChatScreen = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [noResultsFound, setNoResultsFound] = useState(true);
+
   useEffect(() => {
     navigation.setOptions({
       headerLeft: () => {
@@ -30,6 +35,30 @@ export const NewChatScreen = ({ navigation }) => {
           onChangeText={() => {}}
         />
       </View>
+      {!isLoading && !users && (
+        <View style={commonStyles.center}>
+          <FontAwesome
+            name='users'
+            size={55}
+            color={colors.lightGrey}
+            style={styles.noResultsIcon}
+          />
+          <Text style={styles.noResultsText}>
+            Enter a name to search for a user!
+          </Text>
+        </View>
+      )}
+      {!isLoading && noResultsFound && (
+        <View style={commonStyles.center}>
+          <FontAwesome
+            name='question'
+            size={55}
+            color={colors.lightGrey}
+            style={styles.noResultsIcon}
+          />
+          <Text style={styles.noResultsText}>No users found!</Text>
+        </View>
+      )}
     </PageContainer>
   );
 };
@@ -49,5 +78,13 @@ const styles = StyleSheet.create({
     marginLeft: 8,
     fontSize: 15,
     width: '100%',
+  },
+  noResultsIcon: {
+    marginBottom: 20,
+  },
+  noResultsText: {
+    color: colors.textColor,
+    fontFamily: 'regular',
+    letterSpacing: 0.3,
   },
 });
