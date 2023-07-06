dc4940b50fffa028a3d9ee3db219f850f1657c3d
Roman Severyn <romaseverin50@gmail.com>
2023-07-06 19:36:47 +0300

    update ProfileImage component

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -17,11 +17,22 @@ import { updateSignedInUserData } from '../utils/actions/authAction';
 import { useDispatch } from 'react-redux';
 import { updateLoggedInUserData } from '../store/authSlice';
 
-export const ProfileImage = ({ size, uri, userId, showEditButton }) => {
+export const ProfileImage = ({
+  size,
+  uri,
+  userId,
+  showEditButton,
+  showRemoveButton,
+  onPress,
+  style,
+}) => {
   const dispatch = useDispatch();
   const source = uri ? { uri: uri } : userImage;
   const [image, setImage] = useState(source);
   const [isLoading, setIsLoading] = useState(false);
+
+  const isShowEditButton = showEditButton && showEditButton === true;
+  const isShowRemoveButton = showRemoveButton && showRemoveButton === true;
 
   const pickImage = async () => {
     try {
@@ -49,9 +60,9 @@ export const ProfileImage = ({ size, uri, userId, showEditButton }) => {
     }
   };
 
-  const Container = showEditButton ? TouchableOpacity : View;
+  const Container = onPress || isShowEditButton ? TouchableOpacity : View;
   return (
-    <Container onPress={pickImage}>
+    <Container style={style} onPress={onPress || pickImage}>
       {isLoading ? (
         <View style={styles.loadingContainer}>
           <ActivityIndicator size='small' color={colors.primary} />
@@ -63,9 +74,15 @@ export const ProfileImage = ({ size, uri, userId, showEditButton }) => {
         />
       )}
 
-      {!isLoading && showEditButton && (
+      {!isLoading && isShowEditButton && (
         <View style={styles.editIconContainer}>
           <FontAwesome name='pencil' size={16} color='black' />
+        </View>
+      )}
+
+      {!isLoading && isShowRemoveButton && (
+        <View style={styles.removeIconContainer}>
+          <FontAwesome name='close' size={16} color='black' />
         </View>
       )}
     </Container>
@@ -82,6 +99,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 0,
     bottom: -5,
+  },
+  removeIconContainer: {
+    position: 'absolute',
+    right: -3,
+    bottom: -3,
   },
   loadingContainer: {
     justifyContent: 'center',
