0dfea9e8a69262c5117db9d475525d4f29b4e1de
Roman Severyn <romaseverin50@gmail.com>
2023-07-10 18:11:24 +0300

    displaying common chats

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -4,10 +4,29 @@ import { PageContainer } from '../components/PageContainer';
 import { ProfileImage } from '../components/ProfileImage';
 import { PageTitle } from '../components/PageTitle';
 import colors from '../constants/colors';
+import { getUserChats } from '../utils/actions/userActions';
+import { useEffect, useState } from 'react';
+import { DataItem } from '../components/DataItem';
 
 export const ContactScreen = ({ route }) => {
   const storedUsers = useSelector((state) => state.users.storedUsers);
   const currentUser = storedUsers[route.params.uid];
+
+  const storedChats = useSelector((state) => state.chats.chatsData);
+  const [commonChats, setCommonChats] = useState([]);
+
+  useEffect(() => {
+    const getCommonUserChats = async () => {
+      const currentUserChats = await getUserChats(currentUser.userId);
+      setCommonChats(
+        Object.values(currentUserChats).filter(
+          (cid) => storedChats[cid] && storedChats[cid].isGroupChat,
+        ),
+      );
+    };
+
+    getCommonUserChats();
+  }, []);
 
   return (
     <PageContainer>
@@ -25,6 +44,24 @@ export const ContactScreen = ({ route }) => {
           </Text>
         )}
       </View>
+      {commonChats.length > 0 && (
+        <>
+          <Text style={styles.heading}>
+            {commonChats.length} {commonChats.length === 1 ? 'Group' : 'Groups'}{' '}
+            in Common
+          </Text>
+          {commonChats.map((cid) => {
+            const chatData = storedChats[cid];
+            return (
+              <DataItem
+                key={cid}
+                title={chatData.chatName}
+                subTitle={chatData.latestMessageText}
+              />
+            );
+          })}
+        </>
+      )}
     </PageContainer>
   );
 };
@@ -40,5 +77,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     letterSpacing: 0.3,
     color: colors.grey,
+  },
+  heading: {
+    fontFamily: 'bold',
+    letterSpacing: 0.3,
+    color: colors.textColor,
+    marginVertical: 8,
   },
 });
