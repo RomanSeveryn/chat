638aae27038659a553fc95ab33eb1b3726c04f26
Roman Severyn <romaseverin50@gmail.com>
2023-07-12 20:42:28 +0300

    update name in group chat

--- src/screens/ChatSettingsScreen.js
+++ src/screens/ChatSettingsScreen.js
@@ -1,13 +1,63 @@
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { useCallback, useReducer, useState } from 'react';
+import { ActivityIndicator, ScrollView, StyleSheet, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { PageContainer } from '../components/PageContainer';
 import { PageTitle } from '../components/PageTitle';
 import { ProfileImage } from '../components/ProfileImage';
+import { Input } from '../components/Input';
+import { reducer } from '../reducers/formReducer';
+import { validateInput } from '../utils/actions/formActions';
+import { updateChatData } from '../utils/actions/chatActions';
+import { SubmitButton } from '../components/SubmitButton';
+import colors from '../constants/colors';
 
 export const ChatSettingsScreen = ({ route }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   const chatId = route.params.chatId;
   const chatData = useSelector((state) => state.chats.chatsData[chatId]);
   const userData = useSelector((state) => state.auth.userData);
+
+  const initialState = {
+    inputValues: { chatName: chatData.chatName },
+    inputValidities: { chatName: undefined },
+    formIsValid: false,
+  };
+
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+  const inputChangedHandler = useCallback(
+    (inputId, inputValue) => {
+      const result = validateInput(inputId, inputValue);
+      dispatchFormState({ inputId, validationResult: result, inputValue });
+    },
+    [dispatchFormState],
+  );
+
+  const saveHandler = useCallback(async () => {
+    const updatedValues = formState.inputValues;
+
+    try {
+      setIsLoading(true);
+      await updateChatData(chatId, userData.userId, updatedValues);
+
+      setShowSuccessMessage(true);
+
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [formState]);
+
+  const hasChanges = () => {
+    const currentValues = formState.inputValues;
+    return currentValues.chatName != chatData.chatName;
+  };
 
   return (
     <PageContainer>
@@ -22,7 +72,28 @@ export const ChatSettingsScreen = ({ route }) => {
           uri={chatData.chatImage}
         />
 
-        <Text>{chatData.chatName}</Text>
+        <Input
+          id='chatName'
+          label='Chat name'
+          autoCapitalize='none'
+          initialValue={chatData.chatName}
+          allowEmpty={false}
+          onInputChanged={inputChangedHandler}
+          errorText={formState.inputValidities['chatName']}
+        />
+
+        {isLoading ? (
+          <ActivityIndicator size={'small'} color={colors.primary} />
+        ) : (
+          hasChanges() && (
+            <SubmitButton
+              title='Save changes'
+              color={colors.primary}
+              onPress={saveHandler}
+              disabled={!formState.formIsValid}
+            />
+          )
+        )}
       </ScrollView>
     </PageContainer>
   );
