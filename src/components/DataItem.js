f7657ec7d9585a806bbd3bd24d146de21195c02f
Roman Severyn <romaseverin50@gmail.com>
2023-07-13 22:09:33 +0300

    add icon in DataItem

--- src/components/DataItem.js
+++ src/components/DataItem.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import { ProfileImage } from './ProfileImage';
 import colors from '../constants/colors';
+
+const imageSize = 40;
 
 export const DataItem = ({
   title,
@@ -10,20 +12,29 @@ export const DataItem = ({
   onPress,
   type,
   isChecked,
+  icon,
 }) => {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.container}>
-        <ProfileImage uri={image} size={40} />
+        {!icon && <ProfileImage uri={image} size={imageSize} />}
+
+        {icon && (
+          <View style={styles.leftIconContainer}>
+            <AntDesign name={icon} size={20} color={colors.primary} />
+          </View>
+        )}
 
         <View style={styles.textContainer}>
           <Text numberOfLines={1} style={styles.title}>
             {title}
           </Text>
 
-          <Text numberOfLines={1} style={styles.subTitle}>
-            {subTitle}
-          </Text>
+          {subTitle && (
+            <Text numberOfLines={1} style={styles.subTitle}>
+              {subTitle}
+            </Text>
+          )}
         </View>
         {type === 'checkbox' && (
           <View
@@ -72,5 +83,13 @@ const styles = StyleSheet.create({
   checkedStyle: {
     backgroundColor: colors.primary,
     borderColor: 'transparent',
+  },
+  leftIconContainer: {
+    backgroundColor: colors.extraLightGrey,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: imageSize,
+    height: imageSize,
   },
 });
